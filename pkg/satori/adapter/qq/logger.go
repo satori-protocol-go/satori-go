1603b618c074eb0ff0f621289dd57c97d059249a
WindowsSov8forUs <qwertyuiop2333@hotmail.com>
2026-03-29 18:59:16 +0800

    fix: 添加缺失的 botgo 日志注册

--- pkg/satori/adapter/qq/logger.go
+++ pkg/satori/adapter/qq/logger.go
@@ -4,13 +4,70 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/WindowsSov8forUs/botgo-plus"
 	"github.com/satori-protocol-go/satori-go/pkg/satori/logging"
 )
+
+type qqLogger struct {
+	logger logging.Logger
+}
+
+func registerQQLogger(logger logging.Logger) {
+	if logger == nil {
+		logger = logging.NopLogger{}
+	}
+	botgo.SetLogger(&qqLogger{logger: logger})
+}
+
+func (l *qqLogger) Debug(v ...interface{}) {
+	l.log(context.Background(), logging.LevelDebug, v...)
+}
+
+func (l *qqLogger) Info(v ...interface{}) {
+	l.log(context.Background(), logging.LevelDebug, v...)
+}
+
+func (l *qqLogger) Warn(v ...interface{}) {
+	l.log(context.Background(), logging.LevelDebug, v...)
+}
+
+func (l *qqLogger) Error(v ...interface{}) {
+	l.log(context.Background(), logging.LevelDebug, v...)
+}
+
+func (l *qqLogger) Debugf(format string, v ...interface{}) {
+	l.log(context.Background(), logging.LevelDebug, fmt.Sprintf(format, v...))
+}
+
+func (l *qqLogger) Infof(format string, v ...interface{}) {
+	l.log(context.Background(), logging.LevelDebug, fmt.Sprintf(format, v...))
+}
+
+func (l *qqLogger) Warnf(format string, v ...interface{}) {
+	l.log(context.Background(), logging.LevelDebug, fmt.Sprintf(format, v...))
+}
+
+func (l *qqLogger) Errorf(format string, v ...interface{}) {
+	l.log(context.Background(), logging.LevelDebug, fmt.Sprintf(format, v...))
+}
+
+func (l *qqLogger) Sync() error {
+	return nil
+}
+
+func (l *qqLogger) log(ctx context.Context, level logging.Level, v ...interface{}) {
+	if l == nil || l.logger == nil {
+		return
+	}
+	message := fmt.Sprintf("[botgo-plus] %s", fmt.Sprint(v...))
+	l.logger.Log(ctx, level, message)
+}
 
 func (a *Adapter) RegisterLogger(logger logging.Logger) {
 	if logger == nil {
 		logger = logging.NopLogger{}
 	}
+	registerQQLogger(logger)
 	a.mu.Lock()
 	a.logger = logger
 	a.mu.Unlock()
